9bc3a7ff61b2666dc27d400638f04456a63af8df
Vanesa <57409926+vanesa33@users.noreply.github.com>
2021-02-09 14:37:23 -0300

    practica en consola de C sharp, para principiantes
    utilizando switch case

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte dia;
+
+            Console.WriteLine("dame un día: ");
+            dia = Convert.ToByte(Console.ReadLine());
+
+            switch(dia)
+            {
+                case 1: Console.WriteLine("haz elegido lunes"); break;
+                case 2: Console.WriteLine("has elegido martes"); break;
+                case 3: Console.WriteLine("has elegido miercoles"); break;
+                case 4: Console.WriteLine("has elegido jueves"); break;
+                case 5: Console.WriteLine("haz elegido viernes"); break;
+                case 6: Console.WriteLine("has elegido sabado"); break;
+                case 7: Console.WriteLine(" haz elegido domingo"); break;
+                default: Console.WriteLine("no es un dia"); break;
+
+                   
+
+
+            }
+        }
+    }
+}
